4f348b8d0a18289ee11beeb26961a36bceda75f5
guffre <mtguffr@gmail.com>
2019-02-23 18:52:10 -0500

    Cleaned up code; removed globals; made calls more like TCP

--- ggudp.py
+++ ggudp.py
@@ -7,77 +7,68 @@ import time
 import tempfile
 import hashlib
 
-# structs can have different lengths, but Ive found "I" to reliably be 4 bytes.
-# if struct.pack("I", 0) is not length `4`, stuff is going to break.
-# You can replace the struct types here: and in the class struct_pack and struct_unpack
-MISSING_PACKETS  = struct.pack("I", 0x155168C7)
-OUT_OF_RANGE     = struct.pack("I", 0x070F124E)
-DONE_SENDING     = struct.pack("I", 0xD0E53D16)
-LEN_PACKET_NUM   = len(struct.pack("I", 0))
-LEN_MISSING_PKT  = len(MISSING_PACKETS)
-LEN_CHECKSUM     = len(bytearray(hashlib.md5("0").digest()))
-CONNECT_TIMEOUT  = 20
-RECV_LOOP_TIMEOUT = 10 # This should be something low
-TIMEOUT_NO_WAIT = 0.05
-MAX_PACKET_SIZE = 4096
-MIN_DATA_SIZE   = 500
-MAX_DATA_SIZE   = MAX_PACKET_SIZE - LEN_CHECKSUM - LEN_PACKET_NUM
-MAX_RETRY_TIMEOUT = 10
-
 # PACKET:
 #
 #     16          4          X
 # [CHECKSUM][PACKET_NUMBER][DATA]
 
 class GGUdp(object):
-    def __init__(self, ip, port, server=False):
+    def __init__(self, ip, port):
+        self.define_globals()
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.ip = ip
         self.port = port
         self.addr = ((ip,port))
-        self.server = server
-        # If port is specified, this means its a server
-        if server:
-            self.s.bind((ip, port))
+        self.server = False
+        self.s.setblocking(0)
     
-    def struct_pack(self, data, length=4):
-        if length == 4:
-            return struct.pack("I", data)
+    def bind(self):
+        self.server = True
+        self.s.bind((self.ip,self.port))
     
-    def struct_unpack(self, data, length=4):
-        if length == 4:
-            return struct.unpack("I", data)
+    def connect(self, ip, port):
+        self.addr = ((ip,port))
+
+    def byte_flatten(self, data):
+        ret = bytearray()
+        for n in data:
+            ret += bytearray(n)
+        return ret
+
+    def struct_pack(self, data, format="I"):
+        return bytearray(struct.pack(format, data))
+    
+    def struct_unpack(self, data, format="I"):
+            return struct.unpack(format, data)[0]
     
     def _pack_packet(self, data, packet_index):
         packet_number = self.struct_pack(packet_index)
-        data_chunk    = "{}{}".format(packet_number, data)
-        checksum      = self._checksum(data_chunk)
-        data_chunk    = "{}{}".format(checksum, data_chunk)
+        checksum      = self._checksum(packet_number + data)
+        data_chunk    = "{}{}{}".format(checksum, packet_number, data)
         return data_chunk
     
     def _unpack_packet(self, data):
-        checksum     = bytearray(data[:LEN_CHECKSUM])
         try:
-            packet_index = self.struct_unpack(data[LEN_CHECKSUM:LEN_CHECKSUM+LEN_PACKET_NUM])[0]
+            packet_index_bytes = data[self.LEN_CHECKSUM:self.LEN_CHECKSUM+self.LEN_PACKET_NUM]
+            checksum     = bytearray(data[:self.LEN_CHECKSUM])
+            packet_index = self.struct_unpack(packet_index_bytes)
+            data_chunk   = data[self.LEN_HEADERS:]
+            if self._checksum(packet_index_bytes + data_chunk) == checksum:
+               return (packet_index, data_chunk)
         except:
-            return (-1, bytearray("0"))
-        data_chunk   = data[LEN_CHECKSUM+LEN_PACKET_NUM:]
-        if self._checksum(data[LEN_CHECKSUM:]) == checksum:
-            return (packet_index, data_chunk)
+            pass
         return (-1, bytearray("0"))
     
     def send(self, data):
-        data = bytearray(data)
         if len(data) <= 1:
             data += "pad"
+        data = bytearray(data)
         # SYNC LOOP
-        timeout = 5
-        while timeout <= 15:
+        for _ in range(3):
             self._send(bytearray(str(len(data))))
-            response = self._recv(timeout)
+            response = self._recv(self.SYNC_TIMEOUT)
             if response == bytearray(str(len(data))):
                 break
-            timeout += 5
         else:
             print("Failed to sync with server")
             return False
@@ -88,7 +79,7 @@ class GGUdp(object):
         data_chunks = dict()
         while data_index < len(data):
             # Assemble packet data
-            pktsize    = random.randint(MIN_DATA_SIZE, MAX_DATA_SIZE)
+            pktsize    = random.randint(self.MIN_DATA_SIZE, self.MAX_DATA_SIZE)
             data_chunk = self._pack_packet(data[data_index:data_index+pktsize], packet_index)
             # Add to chunk list
             data_chunks[packet_index] = data_chunk
@@ -97,49 +88,56 @@ class GGUdp(object):
             # Increment counters
             data_index   += pktsize
             packet_index += 1
-            #0.001 = ~1.5MB/s, python wont sleep any shorter than this
+            #0.001 = ~200kb/s, python wont sleep any shorter than this
             # Testing with a 1gig up connection across the world, packet loss is too high to be efficient
-            # So, I lower the speed here. This ends up ~9MB/s and minimal packet loss.
-            if packet_index%5 == 0:
+            # So, I lower the speed here. This ends up ~2.1MB/s and minimal packet loss.
+            if packet_index%10 == 0:
                 time.sleep(0.001)
-        self._send(bytearray(DONE_SENDING))
-        self._send(bytearray(DONE_SENDING))
+        time.sleep(0.1)
+        self._send(self.DONE_SENDING)
+        self._send(self.DONE_SENDING)
         
         # HANDLE REREQUESTS
         retries = 3
         while True:
             while retries:
-                data = self._recv(CONNECT_TIMEOUT)
+                data = self._recv(self.SEND_REREQUEST_TIMEOUT)
                 if data == "TIMEOUT":
                     retries -= 1
                 else: break
-            if data.startswith(MISSING_PACKETS):
-                data = data[LEN_MISSING_PKT:]
+            if data.startswith(self.MISSING_PACKETS):
+                data = data[self.LEN_MISSING_PKT:]
                 if len(data) == 0:
                     break
-                for i in range(0, len(data), LEN_MISSING_PKT):
-                    missing_index_bytes = data[i:i+LEN_MISSING_PKT]
-                    missing_index = self.struct_unpack(missing_index_bytes)[0]
-                    to_send = data_chunks.get(missing_index, OUT_OF_RANGE+missing_index_bytes)
+                count = 0
+                time.sleep(0.1)
+                for i in range(0, len(data), self.LEN_MISSING_PKT):
+                    count += 1
+                    missing_index_bytes = data[i:i+self.LEN_MISSING_PKT]
+                    missing_index = self.struct_unpack(missing_index_bytes)
+                    to_send = data_chunks.get(missing_index, self.OUT_OF_RANGE+missing_index_bytes)
                     self._send(to_send)
-                    if to_send.startswith(OUT_OF_RANGE):
+                    if count%30 == 0:
+                         time.sleep(0.001)
+                    if to_send.startswith(self.OUT_OF_RANGE):
                         break
             else:
                 print("Bad packet order? No missing packets packet missed.")
                 break
 
-        tmp = self._recv(TIMEOUT_NO_WAIT)
+        tmp = self._recv(self.TIMEOUT_NO_WAIT)
         while tmp != "TIMEOUT":
-            tmp = self._recv(TIMEOUT_NO_WAIT)
+            tmp = self._recv(self.TIMEOUT_NO_WAIT)
         return True
     
-    def recv(self, timeout=CONNECT_TIMEOUT):
+    def recv(self):
         # SYNC LOOP
-        if self.server: # Server should not timeout when receiving
-            self.s.setblocking(1)
-            len_data,self.addr = self.s.recvfrom(MAX_PACKET_SIZE)
+        self.s.setblocking(1)
+        if self.server:
+            len_data,self.addr = self.s.recvfrom(self.MAX_PACKET_SIZE)
         else:
-            len_data = self._recv(timeout)
+            len_data,_ = self.s.recvfrom(self.MAX_PACKET_SIZE)
+        self.s.setblocking(0)
         try:
             len_data = int(len_data)
             self._send(bytearray(str(len_data)))
@@ -147,11 +145,11 @@ class GGUdp(object):
             return False
         
         # RECEIVE DATA
-        data = dict()
+        data = dict({-1:-1})
         received = 0
         while received < len_data:
-            data_chunk   = self._recv(RECV_LOOP_TIMEOUT)
-            if data_chunk == "TIMEOUT" or data_chunk == DONE_SENDING:
+            data_chunk   = self._recv(self.RECV_LOOP_TIMEOUT)
+            if data_chunk == "TIMEOUT" or data_chunk == self.DONE_SENDING:
                 break
             packet_index,data_chunk = self._unpack_packet(data_chunk)
             if packet_index >= 0:
@@ -160,41 +158,40 @@ class GGUdp(object):
 
         # Clear receive buffer of DONE_SENDING and get missing_packet sync
         try:
-            while data_chunk == DONE_SENDING:
-                data_chunk = self._recv(TIMEOUT_NO_WAIT)
+            while data_chunk == self.DONE_SENDING:
+                data_chunk = self._recv(self.TIMEOUT_NO_WAIT)
         except:
             pass
         # SEND REREQUESTS
         d_max = 0
-        retries = MAX_RETRY_TIMEOUT
-        while (received != len_data) and retries >= 0:
-            missing_packet_max = 1+((len_data - received)/MIN_DATA_SIZE)
-            missing = [MISSING_PACKETS]
+        retries = self.REREQUEST_SAFETY
+        while (received != len_data):
+            missing_packet_max = 1+((len_data - received)/self.MIN_DATA_SIZE)
+            missing = [self.MISSING_PACKETS]
             # Get list of missing chunks
             if not d_max:
-                try:
-                    d_max = max(data)+missing_packet_max
-                except:
-                    d_max = missing_packet_max
+                d_max = max(data)+missing_packet_max
             for i in range(d_max):
                 if i not in data:
                     missing.append(self.struct_pack(i))
-                if (len(missing)*LEN_MISSING_PKT) >= (MAX_DATA_SIZE - LEN_MISSING_PKT): # Be safe, just to avoid off by one
+                if (len(missing)*self.LEN_MISSING_PKT) >= (self.MAX_DATA_SIZE - self.LEN_MISSING_PKT):
                     break
-            self._send(''.join(missing))
+            # Send missing chunk request
+            self._send(self.byte_flatten(missing))
             # If length is 1, no more missing packets!
             if (len(missing) == 1) or (received == len_data):
                 break
+            # Attempt to receive each missing packet
             for i in missing[1:]:
-                data_chunk = self._recv(TIMEOUT_NO_WAIT)
-                if data_chunk.startswith(OUT_OF_RANGE):
-                    d_max = min(d_max, self.struct_unpack(data_chunk[len(OUT_OF_RANGE):])[0])
+                data_chunk = self._recv(self.RECV_REREQUEST_TIMEOUT)
+                if data_chunk.startswith(self.OUT_OF_RANGE):
+                    d_max = min(d_max, self.struct_unpack(data_chunk[len(self.OUT_OF_RANGE):]))
                     break
                 if data_chunk == "TIMEOUT":
                     retries -= 1
                     break
                 else:
-                    retries = MAX_RETRY_TIMEOUT
+                    retries = self.REREQUEST_SAFETY
                     packet_index,data_chunk = self._unpack_packet(data_chunk)
                     if packet_index >= 0:
                         if data.get(packet_index, False):
@@ -203,15 +200,22 @@ class GGUdp(object):
                         data[packet_index] = data_chunk
                 if received == len_data:
                     break
+            if retries == 0:
+                print("Didn't receive all data")
+                break
         else: # No missing packets
-            self._send(str(MISSING_PACKETS))
-            self._send(str(MISSING_PACKETS))
+            self._send(str(self.MISSING_PACKETS))
+            self._send(str(self.MISSING_PACKETS))
 
-        tmp = self._recv(TIMEOUT_NO_WAIT)
+        tmp = self._recv(self.TIMEOUT_NO_WAIT)
         while tmp != "TIMEOUT":
-            tmp = self._recv(TIMEOUT_NO_WAIT)
-
-        return bytearray(''.join(data[n] for n in sorted(data)))
+            tmp = self._recv(self.TIMEOUT_NO_WAIT)
+        
+        if retries == 0:
+            return False
+        
+        _ = data.pop(-1)
+        return self.byte_flatten(data[n] for n in sorted(data))
     
     def _checksum(self, data):
         return bytearray(hashlib.md5(data).digest())
@@ -220,24 +224,57 @@ class GGUdp(object):
         self.s.sendto(data, self.addr)
     
     def _recv(self, timeout):
-        self.s.setblocking(0)
-        ready = select.select([self.s], [], [], timeout)
-        if ready[0]:
-            while True:
-                try:
-                    data,_ = self.s.recvfrom(MAX_PACKET_SIZE)
-                    break
-                except:
-                    print("error")
-                    time.sleep(0.1)
-            self.s.setblocking(1)
-        else:
-            data = "TIMEOUT"
+        data = "TIMEOUT"
+        try:
+            ready = select.select([self.s], [], [], timeout)
+            if ready[0]:
+                data,_ = self.s.recvfrom(self.MAX_PACKET_SIZE)
+        except:
+            pass
         return data
+    
+    def define_globals(self):
+        # Special packets
+        self.MISSING_PACKETS = self.struct_pack(0x155168C7)
+        self.OUT_OF_RANGE    = self.struct_pack(0x070F124E)
+        self.DONE_SENDING    = self.struct_pack(0xD0E53D16)
+        self.LEN_MISSING_PKT = len(self.MISSING_PACKETS)
+
+        # Headers
+        self.LEN_PACKET_NUM = len(self.struct_pack(0))
+        self.LEN_CHECKSUM   = len(self._checksum("0"))
+        self.LEN_HEADERS    = self.LEN_PACKET_NUM + self.LEN_CHECKSUM
+
+        # Packet length info
+        self.MAX_PACKET_SIZE = 4096
+        self.MIN_DATA_SIZE   = 500
+        self.MAX_DATA_SIZE   = self.MAX_PACKET_SIZE - self.LEN_HEADERS
+
+        # Timeout values
+        self.REREQUEST_SAFETY  = 10
+        self.SYNC_TIMEOUT      = 1
+        self.RECV_LOOP_TIMEOUT = 5
+        self.TIMEOUT_NO_WAIT   = 0.05
+        self.SEND_REREQUEST_TIMEOUT = 8 # Must be higher than RECV_REREQUEST_TIMEOUT
+        self.RECV_REREQUEST_TIMEOUT = 0.5
+
+        # Note, REREQUEST_SAFETY * RECV_REREQUEST_TIMEOUT is roughly how much time it takes to TIMEOUT a rerequest loop
+        # If absolutely no packets get received. Each time a successful rerequest comes through though, REREQUEST_SAFETY gets reset
+        # I feel 5 seconds total is generous, but for less reliable networks you might tweak these values
+
+        # Example setup:
+        # For a network with roughly 5 second latency between points, this might be more appropriate:
+        # REREQUEST_SAFETY = 10
+        # SYNC_TIMEOUT = 6
+        # RECV_LOOP_TIMEOUT = 8
+        # TIMEOUT_NO_WAIT = 0.05
+        # SEND_REREQUEST_TIMEOUT = 15
+        # RECV_REREQUEST_TIMEOUT = 3.5
 
 if __name__ == '__main__':
     if sys.argv[1] == "s":
-        s = GGUdp("0.0.0.0", 8000, True)
+        s = GGUdp("0.0.0.0", 8000)
+        s.bind()
         while True:
             data = s.recv()
             if data:
@@ -247,7 +284,7 @@ if __name__ == '__main__':
                     f.write(data)
                     print("File downloaded to: {}".format(tmpfile))
     else:
-        s = GGUdp("127.0.0.1", 8000)
+        s = GGUdp("46.101.242.248", 8000)
         with open(r"X:\cab1.cab", "rb") as f:
             data = f.read()
         s.send(data)
